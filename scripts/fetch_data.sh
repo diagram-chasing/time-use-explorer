cf1b463a6a24caa284539e6c6ba737278f2e1e8d
Vonter <25414711+Vonter@users.noreply.github.com>
2025-05-30 21:11:28 +0530

    chore: update fetch-data to only save required columns

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # Fetch the Parquet file from the data repository
-wget https://github.com/Vonter/india-timeuse-survey/raw/refs/heads/main/data/timeuse.parquet -O src/lib/assets/india_timeuse_survey.parquet
+wget https://github.com/Vonter/india-timeuse-survey/raw/refs/heads/main/data/timeuse.parquet -O src/lib/assets/timeuse.parquet
 
-# Extract a 1% sample of rows from the Parquet file and filter the columns
+# Filter the columns in the original Parquet file
 duckdb -c "COPY (
-  SELECT state, district, gender, age, marital_status, education, religion, 
-         social_group, household_size, activity_code, activity_location, 
-         monthly_expenditure, industry, time_from, time_to,
+  SELECT state, district, gender, age, marital_status, education, religion,
+         sector, social_group, household_size, activity_code,
+         activity_location, monthly_expenditure, industry, time_from, time_to,
+         person_id, mult
+          FROM read_parquet('src/lib/assets/india_timeuse_survey.parquet')
+) TO 'src/lib/assets/india_timeuse_survey.parquet' (FORMAT PARQUET);"
+
+# Extract a 1% sample of rows from the filtered Parquet file
+duckdb -c "COPY (
+  SELECT *
           FROM read_parquet('src/lib/assets/india_timeuse_survey.parquet')
   USING SAMPLE 1%
 ) TO 'src/lib/assets/sample_1_perc.parquet' (FORMAT PARQUET);"
